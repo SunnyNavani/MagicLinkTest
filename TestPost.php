2c84b786d2ba5b8bc7a69878c589c683df918307
zeerox <37295958+SunnyNavani@users.noreply.github.com>
2022-02-24 17:05:56 +0530

    asdf

--- TestPost.php
+++ TestPost.php
@@ -1,11 +1,4 @@
-<html>
-<body>
-
-<form action="welcome.php" method="post">
-Name: <input type="text" name="name"><br>
-E-mail: <input type="text" name="email"><br>
-<input type="submit">
-</form>
-
-</body>
-</html>
+<?php
+$curHighScore = file_get_contents('./highscore.txt');
+echo "Current high score is: " . $curHighScore;
+?>
