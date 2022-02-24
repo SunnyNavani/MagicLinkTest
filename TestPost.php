3caa48d9d4f5b35a41fa53856c035f09852d701e
zeerox <37295958+SunnyNavani@users.noreply.github.com>
2022-02-24 18:52:23 +0530

    asd

--- TestPost.php
+++ TestPost.php
@@ -2,5 +2,6 @@
 $highscoreFile = './highscore.txt';
 $curHighScore = (int) file_get_contents($highscoreFile);
 $curScore = (int) $_POST["curSex"];
+file_put_contents($highscoreFile,$curScore);
 echo "Score ". $curScore;
 echo "HighScore ". $curhighscore;
